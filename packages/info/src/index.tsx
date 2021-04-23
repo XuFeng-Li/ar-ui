b3a0e60dc8fa67e591e58ab688b9226e4c1a2f77
lucky1204 <1125105110@qq.com>
2021-04-23 15:23:33 +0800

    info

--- packages/info/src/index.tsx
+++ packages/info/src/index.tsx
@@ -1,11 +1,11 @@
-import React, {Fragment, useState} from 'react';
-import {isStr, simplifyFileName, isFn, trimSpace} from '@ar/util'
+import React, {CSSProperties, Fragment, useState} from 'react';
+import {simplifyFileName, isFn, trimSpace} from '@ar/util'
 
 /**
  * 参数是否为 0
  * @param {number} val 传入的参数
  * */
-const isZero = (val?:number) => {
+const isZero = (val?: number) => {
   if (val === undefined) return false;
   return `${val}` === '0'
 }
@@ -15,18 +15,31 @@ const isZero = (val?:number) => {
  * @property {string} info 平方米信息
  * */
 export interface SquareMeterInfoProps {
-  info?:string
+  /**
+   * 平方米数据
+   * @property {string} info
+   * */
+  info?: string
 }
+
 /**
  * @typedef {React.FC<SquareMeterInfoProps>>} 定义平方米信息的组件
  * @param {SquareMeterInfoProps} props 平方米信息参数
  * */
-export const SquareMeterInfo:React.FC<SquareMeterInfoProps> = (...props) => {
+export const SquareMeterInfo: React.FC<SquareMeterInfoProps> = ({...props}) => {
   const {info} = props;
   return (<span>{info || '---'}&nbsp;{info && '㎡'}</span>)
 }
 
-const defaultKayMap = {
+export interface ARHouseTypeKeyMap {
+  bedroomNum?: string,
+  livingRoomNum?: string,
+  kitchenNum?: string,
+  bathroomNum?: string,
+  balconyNum?: string
+}
+
+const defaultKayMap: ARHouseTypeKeyMap = {
   bedroomNum: 'bedroomNum',
   livingRoomNum: 'livingRoomNum',
   kitchenNum: 'kitchenNum',
@@ -34,33 +47,68 @@ const defaultKayMap = {
   balconyNum: 'balconyNum'
 };
 
-export const HouseType = (props) => {
+export interface HouseTypeProps {
+  data?: any,
+  map?: ARHouseTypeKeyMap
+}
+
+export const HouseType: React.FC<HouseTypeProps> = ({...props}) => {
   const {data, map = {}} = props;
-  const keyMap = {...defaultKayMap, ...map};
-  const bedroom = data[keyMap.bedroomNum] || isZero(data[keyMap.bedroomNum]) ? data[keyMap.bedroomNum] : '--'
-  const livingRoom = data[keyMap.livingRoomNum] || isZero(data[keyMap.livingRoomNum]) ? data[keyMap.livingRoomNum] : '--'
-  const kitchen = data[keyMap.kitchenNum] || isZero(data[keyMap.kitchenNum]) ? data[keyMap.kitchenNum] : '--'
-  const bathroom = data[keyMap.bathroomNum] || isZero(data[keyMap.bathroomNum]) ? data[keyMap.bathroomNum] : '--'
-  const balconyNum = data[keyMap.balconyNum] || isZero(data[keyMap.balconyNum]) ? data[keyMap.balconyNum] : '--'
+  if (!data || typeof data === 'object') {
+    return (<span/>);
+  }
+  const keyMap: ARHouseTypeKeyMap = {...defaultKayMap, ...map};
+  const bedroom = data[keyMap.bedroomNum!] || isZero(data[keyMap.bedroomNum!]) ? data[keyMap.bedroomNum!] : '--'
+  const livingRoom = data[keyMap.livingRoomNum!] || isZero(data[keyMap.livingRoomNum!]) ? data[keyMap.livingRoomNum!] : '--'
+  const kitchen = data[keyMap.kitchenNum!] || isZero(data[keyMap.kitchenNum!]) ? data[keyMap.kitchenNum!] : '--'
+  const bathroom = data[keyMap.bathroomNum!] || isZero(data[keyMap.bathroomNum!]) ? data[keyMap.bathroomNum!] : '--'
+  const balconyNum = data[keyMap.balconyNum!] || isZero(data[keyMap.balconyNum!]) ? data[keyMap.balconyNum!] : '--'
   return (<span>{`${bedroom}室${livingRoom}厅${kitchen}厨${bathroom}卫${balconyNum}阳台`}</span>)
 }
 
-const defaultAreaKayMap = {provinceName: 'provinceName', cityName: 'cityName', districtName: 'districtName'};
-const defaultAreaKayMapTwo = {provinceName: 'province', cityName: 'city', districtName: 'district'};
+export interface ARAreaKeyMap {
+  provinceName?: string,
+  cityName?: string,
+  districtName?: string
+}
 
-export const AreaInfo = (props) => {
+const defaultAreaKayMap: ARAreaKeyMap = {
+  provinceName: 'provinceName',
+  cityName: 'cityName',
+  districtName: 'districtName'
+};
+const defaultAreaKayMapTwo: ARAreaKeyMap = {
+  provinceName: 'province',
+  cityName: 'city',
+  districtName: 'district'
+};
+
+export interface AreaInfoProps {
+  data?: any,
+  map?: ARAreaKeyMap,
+  address?: string,
+  mode?: 'keyNoName' | string,
+}
+
+export const AreaInfo: React.FC<AreaInfoProps> = ({...props}) => {
   const {data, map = {}, address, mode = 'keyNoName'} = props;
   let keyMap = {...defaultAreaKayMap, ...map};
   if (mode === 'keyNoName') {
     keyMap = {...defaultAreaKayMapTwo, ...map}
   }
-  const province = data[keyMap.provinceName] ? data[keyMap.provinceName] : '--'
-  const city = data[keyMap.cityName] ? data[keyMap.cityName] : '--'
-  const district = data[keyMap.districtName] ? data[keyMap.districtName] : ''  // 区可能没有
-  return (<span>{`${province}${city}${district}`}&nbsp;&nbsp;{address || null}</span>)
+  const province = data[keyMap.provinceName!] ? data[keyMap.provinceName!] : '--'
+  const city = data[keyMap.cityName!] ? data[keyMap.cityName!] : '--'
+  const district = data[keyMap.districtName!] ? data[keyMap.districtName!] : ''  // 区可能没有
+  return (<span>{`${province}${city}${district}`}&nbsp;&nbsp;{address || ''}</span>)
 }
 
-export const UpLoadInfo = (props) => {
+export interface UpLoadInfoProps {
+  noHref: boolean,
+  style?: CSSProperties,
+  data?: string | string[],
+}
+
+export const UpLoadInfo: React.FC<UpLoadInfoProps> = ({...props}) => {
   const {noHref, style = {}} = props;
   let {data} = props;
   if (!data || !data.length) return null;
@@ -78,15 +126,28 @@ export const UpLoadInfo = (props) => {
             extendStyle.target = '_blank'
           }
           return (
-            <a href={noHref ? 'javascript:void(0);' : ele} key={-i} rel="noopener noreferrer"><img src={ele} alt='pic'
-                                                                                                   width='86px'
-                                                                                                   style={{...extendStyle}}/></a>)
+            <a
+              href={noHref ? 'javascript:void(0);' : ele}
+              key={-i}
+              rel="noopener noreferrer">
+              <img
+                src={ele}
+                alt='pic'
+                width='86px'
+                style={{...extendStyle}}
+              />
+            </a>)
         })
       }
     </Fragment>)
 }
 
-export const VideoList = (props) => {
+export interface VideoListProps {
+  style?: CSSProperties,
+  list?: string | string[]
+}
+
+export const VideoList: React.FC<VideoListProps> = ({...props}) => {
   const {style = {}} = props;
   let {list} = props;
   if (!list || !list.length) return null;
@@ -103,34 +164,88 @@ export const VideoList = (props) => {
             extendStyle.style.marginLeft = '15px'
           }
           extendStyle = {...extendStyle, ...style}
-          return (<video key={ele} style={{display: 'inline-block', ...extendStyle}} src={ele} controls="controls"/>)
+          return (
+            <video
+              key={ele}
+              style={{display: 'inline-block', ...extendStyle}}
+              src={ele}
+              controls
+            />
+          )
         })
       }
     </Fragment>)
 }
 
-export const MainPic = (props) => {
+export interface MainPicProps {
+  style?: CSSProperties,
+  url?: string,
+  isPrivate?: boolean,
+}
+
+export const MainPic: React.FC<MainPicProps> = ({...props}) => {
   const {style = {}, url, isPrivate} = props;
   const {width = 100, height = 86} = style;
   const w = `${width}`.replace(/px/, '');
   const h = `${height}`.replace(/px/, '');
-
   if (url && isPrivate) {
-    return <a href={url} rel="noopener noreferrer" target="_blank"><img src={`${url}`} alt='pic' height='100px'
-                                                                        width='86px' style={{...style}}/></a>
+    return (
+      <a
+        href={url}
+        rel="noopener noreferrer"
+        target="_blank">
+        <img
+          src={`${url}`}
+          alt='pic'
+          height='100px'
+          width='86px'
+          style={{...style}}
+        />
+      </a>
+    )
   }
-
   if (url) {
-    return <a href={url} rel="noopener noreferrer" target="_blank"><img
-      src={`${url}?x-oss-process=image/resize,m_fixed,h_${h},w_${w}`} alt='pic' height='100px' width='86px'
-      style={{...style}}/></a>
+    return (
+      <a
+        href={url}
+        rel="noopener noreferrer"
+        target="_blank">
+        <img
+          src={`${url}?x-oss-process=image/resize,m_fixed,h_${h},w_${w}`}
+          alt='pic'
+          height='100px'
+          width='86px'
+          style={{...style}}
+        />
+      </a>
+    )
   }
-  return <a href='javascript:void(0);' rel="noopener noreferrer" target="_blank"><img
-    src='https://asman-img.oss-cn-hangzhou.aliyuncs.com/noPic_0e7bffac7958f603a8b37fe3cda07499.png' alt='pic'
-    height='100px' width='86px' style={{...style}}/></a>
+  return (
+    <a
+      href='javascript:void(0);'
+      rel="noopener noreferrer"
+      target="_blank">
+      <img
+        src='https://asman-img.oss-cn-hangzhou.aliyuncs.com/noPic_0e7bffac7958f603a8b37fe3cda07499.png'
+        alt='pic'
+        height='100px'
+        width='86px'
+        style={{...style}}
+      />
+    </a>
+  )
 }
 
-export const UploadPreview = (props) => {
+export interface UploadPreviewProps {
+  style?: CSSProperties,
+  innerStyle?: CSSProperties,
+  params?: any,
+  height: number,
+  value: any,
+  list?: any[],
+}
+
+export const UploadPreview: React.FC<UploadPreviewProps> = ({...props}) => {
   const {value, height} = props;
   // eslint-disable-next-line prefer-const
   let {list = [], params = {}, style = {}, innerStyle = {}, ...rest} = props;
@@ -140,11 +255,17 @@ export const UploadPreview = (props) => {
   }
   if (!list) return null;
   const showImagePreview = () => {
+    const tempWindow: any = window;
     // eslint-disable-next-line no-underscore-dangle
-    window.g_app._store.dispatch({
+    tempWindow['g_app']['_store'].dispatch({
       type: 'global/showImagePreviewVisiable',
       payload: {list, params}
-    });
+    })
+    // eslint-disable-next-line no-underscore-dangle
+    // window.g_app._store.dispatch({
+    //   type: 'global/showImagePreviewVisiable',
+    //   payload: {list, params}
+    // });
   }
 
   innerStyle = height == null ? {...innerStyle} : {...innerStyle, height}
@@ -160,25 +281,45 @@ export const UploadPreview = (props) => {
   )
 }
 
-export const UrlLink = (props) => {
+export interface UrlLinkProps {
+  list?: string[] | string
+}
+
+export const UrlLink: React.FC<UrlLinkProps> = ({...props}) => {
   let {list} = props;
-  if (!list) return;
-  if (isStr(list)) {
+  if (!list || list.length <= 0) return null;
+  if (typeof list === 'string') {
     list = [list]
   }
   // eslint-disable-next-line consistent-return
-  return list.map(ele => {
-    const url = simplifyFileName(ele,1);
-    return <div><a href={ele} rel="noopener noreferrer" target="_blank">{url}</a></div>
-  })
+  return (
+    <Fragment>
+      {
+        list.map(ele => {
+          const url = simplifyFileName(ele, 1);
+          return <div><a href={ele} rel="noopener noreferrer" target="_blank">{url}</a></div>
+        })
+      }
+    </Fragment>
+  )
 }
 
-export const BlankLink = (props) => {
+export interface BlankLinkProps {
+  title?: string,
+  href: string,
+}
+
+export const BlankLink: React.FC<BlankLinkProps> = ({...props}) => {
   const {href, title} = props;
   return <a href={href} rel="noopener noreferrer" target="_blank">{title || href}</a>
 }
 
-export const TitleInfo = (props) => {
+export interface TitleInfoProps {
+  info: string,
+  len?: number,
+}
+
+export const TitleInfo: React.FC<TitleInfoProps> = ({...props}) => {
   const {len, info, ...rest} = props;
   let tit = info;
   if (info.length && len && info.length >= len) {
@@ -187,31 +328,47 @@ export const TitleInfo = (props) => {
   return <span {...rest} title={info}>{tit}</span>
 }
 
-export const BuleWrapper = (props) => {
+export interface BuleWrapperProps {
+  info?: string,
+  text?: string,
+  color?: string
+}
+
+export const BuleWrapper: React.FC<BuleWrapperProps> = ({...props}) => {
   const {info, text, color = '#0066FF'} = props;
   if (!info) return null;
   return <span><span style={{color}}>{info}</span>{text}</span>
 }
 
-export const Bule = (props) => {
+export interface BuleProps {
+  info?: string,
+  color?: string
+}
+
+export const Bule: React.FC<BuleProps> = ({...props}) => {
   const {info, color = '#0066FF'} = props;
   if (!info) return null;
   return <span style={{color}}>{info}</span>
 }
 
-export const Red = (props) => {
+export interface RedProps {
+  info?: string,
+  color?: string
+}
+
+export const Red: React.FC<RedProps> = ({...props}) => {
   const {info, color = '#FF0000'} = props;
   if (!info) return null;
   return <span style={{color}}>{info}</span>
 }
 
 
-export const getColorWrapper = (props) => {
+export const getColorWrapper = (props: { info: any }) => {
   const {info} = props;
   return info ? <BuleWrapper {...props} /> : null;
 }
 
-export const ShowMoreInfo = (props) => {
+export const ShowMoreInfo = (props: { info?: string, len: number }) => {
   const {info, len} = props;
   let initValue = '';
   let needSubstring = false;
@@ -219,14 +376,14 @@ export const ShowMoreInfo = (props) => {
     initValue = `${info.substring(0, len)}...`;
     needSubstring = true;
   } else {
-    initValue = info
+    initValue = info || '';
   }
   const [showInfo, setShowInfo] = useState(initValue);
 
   const isShowAll = () => showInfo === info
 
   const showMore = () => {
-    setShowInfo(isShowAll() ? initValue : info)
+    setShowInfo(isShowAll() ? initValue : (info || ''))
   }
 
   return (
@@ -235,34 +392,51 @@ export const ShowMoreInfo = (props) => {
   )
 }
 
+export interface HouseDescProps {
+  buildingNo?: string,
+  unitNo?: string,
+  roomNo?: string,
+}
+
 /**
  * xx幢xxx单元xxxx
  * @param {*} props
  */
-export const HouseDesc = (props) => {
+export const HouseDesc: React.FC<HouseDescProps> = ({...props}) => {
   const {buildingNo, unitNo, roomNo} = props;
   return (<span>{`${buildingNo || '--'}幢${unitNo || '--'}单元${roomNo || '--'}室`}</span>)
 }
 
 
-export const SomeRed = (props) => {
+export const SomeRed = (props: { info: string, keyWord: string }) => {
   const {info, keyWord} = props;
   if (!info || !keyWord) return info;
-  const trimkey = trimSpace(keyWord);
-  const subIndex = info.indexOf(trimkey);
+  const aTrimkey = trimSpace(keyWord);
+  const subIndex = info.indexOf(aTrimkey);
   if (subIndex === -1) return info;
   const start = info.substring(0, subIndex);
-  const end = info.substring(subIndex + trimkey.length);
+  const end = info.substring(subIndex + aTrimkey.length);
   return (<span>{start}<span style={{color: 'red'}}>{keyWord}</span>{end}</span>)
 }
 
-export const LRInfo = (props) => {
+export interface LRInfoProps {
+  title?: string,
+  info?: number,
+  width?: number,
+  hasColon?: boolean,
+  len?: number,
+  onClick?: Function,
+  titleStyle?: CSSProperties,
+
+}
+
+export const LRInfo: React.FC<LRInfoProps> = ({...props}) => {
   const {title, info, width, hasColon = true, onClick, len} = props;
   let {titleStyle = {}} = props;
   if (len) {
     titleStyle = {...titleStyle, width: `${30 + 12 * len}px`, textAlign: 'right'}
   }
-  const titleProps = {
+  const titleProps: any = {
     style: width ? {...titleStyle, width, textAlign: 'right'} : {...titleStyle}
   }
   let infoProps = {}
@@ -272,17 +446,29 @@ export const LRInfo = (props) => {
   if (!title) {
     return <div {...infoProps}>{info}</div>
   }
-  return (<div className='df'>
-    <div className='g0' {...titleProps}>{title || ' -- '}&nbsp;{hasColon ? ':' : null} &nbsp;</div>
-    <div className='df flex1' {...infoProps}>{info !== 0 ? info || '---' : info}</div>
-  </div>)
+  return (
+    <div className='df'>
+      <div className='g0' {...titleProps}>
+        {title || ' -- '}&nbsp;{hasColon ? ':' : ''} &nbsp;
+      </div>
+      <div className='df flex1' {...infoProps}>{info !== 0 ? info || '---' : info}</div>
+    </div>
+  )
 }
 
-export const BuildingArea = (props) => {
+export interface BuildingAreaProps {
+  data?: {
+    buildingName?: string,
+    houseCode?: string,
+    buildingNo?: string,
+    unitNo?: string,
+    roomNo?: string
+  }
+}
+
+export const BuildingArea: React.FC<BuildingAreaProps> = ({...props}) => {
   const {data} = props;
   const {buildingName, houseCode, buildingNo, unitNo, roomNo} = data || {}
   return <span>{`${buildingName || '--'}${houseCode ? `${houseCode}户型` : ''}`}
     <br/> {buildingNo ? `${buildingNo || '--'}栋${unitNo || '--'}单元${roomNo || '--'}室` : ''}</span>
 }
-
-export default LRInfo;
---
+++ packages/info/types/index.d.js
@@ -0,0 +1,141 @@
+import React, { CSSProperties } from 'react';
+
+/**
+ * @typedef {object} SquareMeterInfoProps 定义平方米信息的组件参数
+ * @property {string} info 平方米信息
+ * */
+interface SquareMeterInfoProps {
+    /**
+     * 平方米数据
+     * @property {string} info
+     * */
+    info?: string;
+}
+/**
+ * @typedef {React.FC<SquareMeterInfoProps>>} 定义平方米信息的组件
+ * @param {SquareMeterInfoProps} props 平方米信息参数
+ * */
+declare const SquareMeterInfo: React.FC<SquareMeterInfoProps>;
+interface ARHouseTypeKeyMap {
+    bedroomNum?: string;
+    livingRoomNum?: string;
+    kitchenNum?: string;
+    bathroomNum?: string;
+    balconyNum?: string;
+}
+interface HouseTypeProps {
+    data?: any;
+    map?: ARHouseTypeKeyMap;
+}
+declare const HouseType: React.FC<HouseTypeProps>;
+interface ARAreaKeyMap {
+    provinceName?: string;
+    cityName?: string;
+    districtName?: string;
+}
+interface AreaInfoProps {
+    data?: any;
+    map?: ARAreaKeyMap;
+    address?: string;
+    mode?: 'keyNoName' | string;
+}
+declare const AreaInfo: React.FC<AreaInfoProps>;
+interface UpLoadInfoProps {
+    noHref: boolean;
+    style?: CSSProperties;
+    data?: string | string[];
+}
+declare const UpLoadInfo: React.FC<UpLoadInfoProps>;
+interface VideoListProps {
+    style?: CSSProperties;
+    list?: string | string[];
+}
+declare const VideoList: React.FC<VideoListProps>;
+interface MainPicProps {
+    style?: CSSProperties;
+    url?: string;
+    isPrivate?: boolean;
+}
+declare const MainPic: React.FC<MainPicProps>;
+interface UploadPreviewProps {
+    style?: CSSProperties;
+    innerStyle?: CSSProperties;
+    params?: any;
+    height: number;
+    value: any;
+    list?: any[];
+}
+declare const UploadPreview: React.FC<UploadPreviewProps>;
+interface UrlLinkProps {
+    list?: string[] | string;
+}
+declare const UrlLink: React.FC<UrlLinkProps>;
+interface BlankLinkProps {
+    title?: string;
+    href: string;
+}
+declare const BlankLink: React.FC<BlankLinkProps>;
+interface TitleInfoProps {
+    info: string;
+    len?: number;
+}
+declare const TitleInfo: React.FC<TitleInfoProps>;
+interface BuleWrapperProps {
+    info?: string;
+    text?: string;
+    color?: string;
+}
+declare const BuleWrapper: React.FC<BuleWrapperProps>;
+interface BuleProps {
+    info?: string;
+    color?: string;
+}
+declare const Bule: React.FC<BuleProps>;
+interface RedProps {
+    info?: string;
+    color?: string;
+}
+declare const Red: React.FC<RedProps>;
+declare const getColorWrapper: (props: {
+    info: any;
+}) => JSX.Element | null;
+declare const ShowMoreInfo: (props: {
+    info?: string;
+    len: number;
+}) => JSX.Element;
+interface HouseDescProps {
+    buildingNo?: string;
+    unitNo?: string;
+    roomNo?: string;
+}
+/**
+ * xx幢xxx单元xxxx
+ * @param {*} props
+ */
+declare const HouseDesc: React.FC<HouseDescProps>;
+declare const SomeRed: (props: {
+    info: string;
+    keyWord: string;
+}) => string | JSX.Element;
+interface LRInfoProps {
+    title?: string;
+    info?: number;
+    width?: number;
+    hasColon?: boolean;
+    len?: number;
+    onClick?: Function;
+    titleStyle?: CSSProperties;
+}
+declare const LRInfo: React.FC<LRInfoProps>;
+interface BuildingAreaProps {
+    data?: {
+        buildingName?: string;
+        houseCode?: string;
+        buildingNo?: string;
+        unitNo?: string;
+        roomNo?: string;
+    };
+}
+declare const BuildingArea: React.FC<BuildingAreaProps>;
+
+export { ARAreaKeyMap, ARHouseTypeKeyMap, AreaInfo, AreaInfoProps, BlankLink, BlankLinkProps, BuildingArea, BuildingAreaProps, Bule, BuleProps, BuleWrapper, BuleWrapperProps, HouseDesc, HouseDescProps, HouseType, HouseTypeProps, LRInfo, LRInfoProps, MainPic, MainPicProps, Red, RedProps, ShowMoreInfo, SomeRed, SquareMeterInfo, SquareMeterInfoProps, TitleInfo, TitleInfoProps, UpLoadInfo, UpLoadInfoProps, UploadPreview, UploadPreviewProps, UrlLink, UrlLinkProps, VideoList, VideoListProps, getColorWrapper };
