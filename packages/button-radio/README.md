f92eaad661333c5a08d41a88970296247aa51853
lucky1204 <1125105110@qq.com>
2021-05-10 16:27:46 +0800

    ARButtonRadio

---
+++ packages/button-radio/README.md
@@ -0,0 +1,11 @@
+# `@ar/button-radio`
+
+> TODO: description
+
+## Usage
+
+```
+const index = require('@ar/button-radio');
+
+// TODO: DEMONSTRATE API
+```
---
+++ packages/button-radio/src/components/ARButtonRadio/index.tsx
@@ -0,0 +1,78 @@
+import React from "react";
+import {Radio} from "antd";
+
+import "antd/dist/antd.css";
+// @ts-ignore
+import styles from "./index.module.scss";
+import {RadioChangeEvent} from "antd/lib/radio/interface";
+
+export type ARRadioButton = 'outline' | 'solid';
+export type ARRadioSize = 'large' | 'middle' | 'small';
+
+const RadioGroup = Radio.Group;
+const RadioButton = Radio.Button;
+
+
+export interface ARButtonRadioProps {
+  options: ARButtonRadioOption[],
+  buttonStyle?: ARRadioButton,
+  disabled?: boolean,
+  size?: ARRadioSize,
+  value?: string,
+  defaultValue?: string,
+  onChange?: (option: ARButtonRadioOption) => void,
+}
+
+export interface ARButtonRadioOption {
+  /**
+   * 选项的值标记
+   * */
+  value: string,
+  /**
+   * 选项的标签
+   * */
+  label: string,
+  /**
+   * 选项是否可交互
+   * */
+  disabled?: boolean,
+}
+
+export const ARButtonRadio: React.FC<ARButtonRadioProps> =
+  ({
+     options = [],
+     buttonStyle = 'outline',
+     onChange,
+     ...props
+   }) => {
+    const onChangeHandler = (option:ARButtonRadioOption) => (_: RadioChangeEvent)=>{
+      if (onChange) {
+        onChange(option);
+      }
+    }
+    return (
+      <RadioGroup
+        className={styles.index}
+        {...props}
+        optionType="button"
+        buttonStyle={buttonStyle}
+      >
+        {
+          options && options.length >= 1 &&
+          options.map((option, index) => {
+            const {label: optionLabel, ...otherProps} = option;
+            return (
+              <RadioButton
+                {...otherProps}
+                key={`radio-group-value-options-${option.value}-${(10000 + index)}`}
+                value={option.value}
+                onChange={onChangeHandler(option)}
+              >
+                {optionLabel}
+              </RadioButton>
+            )
+          })
+        }
+      </RadioGroup>
+    )
+  }
